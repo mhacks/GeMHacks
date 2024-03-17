085a51cbb48e36cf778936b62eb5d420836250ac
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2024-03-17 18:10:13 -0400

    Made logos go away on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import '../Header.css';
 const Header = () => (
         <header>
             <div className="header-content">
-                <img src={process.env.PUBLIC_URL + '/images/logos/mhacks_logo.svg'} alt="MHacksLogo" style={{'marginRight': "auto"}} className="header-logo"/>
+                <img src={process.env.PUBLIC_URL + '/images/logos/mhacks_logo.svg'} alt="MHacksLogo" style={{'marginRight': "auto"}} className="header-logo header-logo-mhacks"/>
                 <nav>
                     <ul className="nav-links flex lg:space-x-20">
                         <li><a href="#about" className="text-white hover:underline-custom font-bold text-xl font-archivo">ABOUT</a></li>
@@ -14,7 +14,7 @@ const Header = () => (
                         <li><a href="#faq" className="text-white hover:underline-custom font-bold font-archivo text-xl">FAQ</a></li>
                     </ul>
                 </nav>
-                <img src={process.env.PUBLIC_URL + '/images/logos/google_logo.svg'} alt="GoogleLogo" style={{'marginLeft': "auto"}} className="header-logo"/>
+                <img src={process.env.PUBLIC_URL + '/images/logos/google_logo.svg'} alt="GoogleLogo" style={{'marginLeft': "auto"}} className="header-logo header-logo-google"/>
             </div>
         </header>
     );
