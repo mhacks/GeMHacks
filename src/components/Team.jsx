768bfe5a93bdad70c6acab52cb0e354b0d452271
XinyuP <xinyup@umich.edu>
2024-03-20 03:27:59 -0400

    add mhacks team members

---
+++ src/components/Team.jsx
@@ -0,0 +1,104 @@
+export default function Team() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="mb-6 text-4xl font-syncopate font-bold">MHACKS TEAM</h1>
+      <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">
+              Directors
+            </h1>
+            <p className="py-5 font-bold font-inter">
+              April Feng, <br />
+              Brian Travis
+              <br /> <br />
+            </p>
+          </div>
+        </div>
+
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">Tech</h1>
+            <p className="py-3 font-bold font-inter">
+              Kevin Wang, <br />
+              Harry Sanders, <br />
+              Arnav Shah, <br />
+              Akira Li, <br />
+              Anish Chandra, <br />
+              Blaire Pang, <br />
+              Jasmine Hou,
+              <br />
+              Gannon Smith
+              <br />
+            </p>
+          </div>
+        </div>
+
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">
+              Sponsorship
+            </h1>
+            <p className="py-5 font-bold font-inter">
+              Mihir Arya, <br /> Derek Miller, <br />
+              Michael West, <br />
+              Michael Kim <br />
+            </p>
+          </div>
+        </div>
+
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">
+              Logistics
+            </h1>
+            <p className="py-5 font-bold font-inter">
+              Zain Zai, <br />
+              Steven Zhang, <br />
+              Amulya Gundlapally <br />
+              <br />
+            </p>
+          </div>
+        </div>
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">Design</h1>
+            <p className="py-5 font-bold font-inter">
+              Renée Thomas, <br />
+              Phoebe Oh, <br />
+              Sarah Bargfrede, <br />
+              Jialan Ren <br />
+            </p>
+          </div>
+        </div>
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">
+              Outreach
+            </h1>
+            <p className="py-5 font-bold font-inter">
+              Desola Fujah, <br />
+              Katie Wang, <br />
+              Julia Mei <br />
+              <br />
+            </p>
+          </div>
+        </div>
+        <div class="p-10 w-64 h-72 border-black border-2 rounded-md shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-xl font-syncopate font-bold text-black">
+              Advisors
+            </h1>
+            <p className="py-5 font-bold font-inter">
+              Adviti Mishra, <br />
+              Spencer Goodwin <br /> <br />
+              <br />
+            </p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+}
