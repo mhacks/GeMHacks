e883b1a876f17f98c20922d7510ac81d15ada822
jsmnhou <jsmnhou@umich.edu>
2024-03-20 20:14:40 -0400

    FAQ completed

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,11 +4,14 @@ export default function FAQ() {
     return (
         <div className="mt-10 flex flex-col items-center justify-center">
             <h1 className="text-4xl font-syncopate font-bold">FAQS</h1>
-            <Accordion className="m-4" question="What is Google x MHacks?" answer="Placeholder." />
-            <Accordion className="m-4" question="How Many Can Be on a Team?" answer="Hackers may form teams of up to 4 people. Teams can be formed before or during the event." />
-            <Accordion className="m-4" question="What is MHacks x Google?" answer="Placeholder." />
-            <Accordion className="m-4" question="What is MHacks x Google?" answer="Placeholder." />
-            <button className="flex cursor-pointer items-center mt-20 mb-20 rounded-md border-2 border-black bg-blue-400 px-10 py-3 font-bold font-archivo shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">Apply Now</button>
+            <Accordion className="m-4" question="Who can apply?" answer="Anyone that is 18+ is welcome to apply!" />
+            <Accordion className="m-4" question="When will this event take place?" answer="The event will be held on April 12th - 14th. Workshops will be held on Friday (4/12) and hacking will be done on Saturday and Sunday." />
+            <Accordion className="m-4" question="Where will this event take place?" answer="Friday’s workshops will be held on Central Campus. The exact building will be announced later. Hacking on Saturday and Sunday will be at the Google Ann Arbor office at 2300 Traverwood Dr, Ann Arbor, MI 48105." />
+            <Accordion className="m-4" question="What will be provided?" answer="Food will be provided! Please note there will not be sleeping arrangements." />
+            <Accordion className="m-4" question="When are applications due?" answer="Applications are due on March 29th at 11:59pm. However, applications are reviewed on a rolling basis so our deadline may close earlier as slots fill up." />
+            <Accordion className="m-4" question="How do teams work?" answer="Teams can be up to 4 members, but acceptances are individual." />
+            <Accordion className="m-4" question="Do I need experience to participate?" answer="While prior experience using AI models in project will help, prior experience is not required! At MHacks 16 last fall, almost half of all hackers had never attended a hackathon before. For this event, we encourage anyone who’s interested to apply." />
+            <Accordion className="m-4" question="What if I still have additional questions??" answer="Send us an email at help@mhacks.org and we'll get back to you shortly :)." />
       </div>
     )
 }
