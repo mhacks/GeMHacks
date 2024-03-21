ad1105cfcfe87d7772b22435246d81bdb103cf67
Derek Miller <derekmil@umich.edu>
2024-03-21 12:01:03 -0400

    reworded description for compliance reasons + cursor pointer on hover for nav items

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,12 @@ export default function About({id}) {
             A few workshops, led by Google engineers and PMs, will be hosted on Friday the 12th, 
             while the hackathon will take place Saturday and Sunday. 
             <br/> <br/>
-            Participants will get <span className="font-bold">exclusive early-access</span> to <span className="font-bold">
-                recently announced Gemini 1.5 API</span> along with a host of other Google AI Studio tools. 
-            Assorted tech talks and workshops accompany a few speakers you don't want to miss. Of course, hackathon swag will be plentiful, 
+            Participants will get <span className="font-bold">access</span> to <span className="font-bold"> Gemini 1.5 API</span> along with a host of other 
+            Google AI Studio tools. Assorted tech talks and workshops accompany a few speakers you <span className="italic"> don't</span> want to miss. Of course, hackathon swag will be plentiful, 
             along with networking opportunities with Google employees and other students. Teams are of up to four, but acceptances are individual. 
             The prize pool is as follows: first prize is $<span className="font-bold">3000</span>, 
             second prize is $<span className="font-bold">1500</span>, and third prize is $<span className="font-bold">500</span>. 
             <br/> <br/>
-
             Food is provided, but there will be no overnight accommodations. 
             Please apply by <span className="font-bold">March 29th, 11:59pm</span>; 
             note that we will be reviewing applications on a <span className="font-bold">rolling basis</span> so 
