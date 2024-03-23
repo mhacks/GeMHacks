19de7a0a6e1ea578c281fedb9f78ef375d163db6
Harry Sanders <harrison.w.sanders@gmail.com>
2024-03-23 13:41:22 -0400

    fixed my mess ups

--- src/components/MovingObjects.jsx
+++ src/components/MovingObjects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Parallax } from 'react-scroll-parallax';
-import '../MovingObjects.css'; 
+import '../MovingObjects.css';
 
 const imagePaths = [
   '/images/abstract/b01.svg',
@@ -17,38 +17,35 @@ const MovingObjects = () => {
   const [objects, setObjects] = useState([]);
 
   useEffect(() => {
-    const generatedObjects = Array.from({ length: 20 }).map((_, index) => {
+    const generatedObjects = Array.from({ length: 20 }).map(() => {
       const imagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
-      const speed = Math.random() * (10) + 80; 
-      const scale = Math.random() * (0.3 - 0.2) + 0.2;
-      const direction = index % 2 === 0 ? 'moveRight' : 'moveLeft';
-      const top = `${Math.random() * (100 - 5) + 5}%`;
-      const left = `${Math.random() * (100 - 5) + 5}%`;
-      return { imagePath, speed, scale, direction, top, left }; 
+      const speed = Math.random() * (10) + 80;
+      const scale = Math.random() * (0.3 - 0.2) + 0.2; 
+      const direction = Math.random() > 0.5 ? 'moveRight' : 'moveLeft'; 
+      const xPos = Math.random() * 100; 
+      return { imagePath, speed, scale, direction, xPos };
     });
 
     setObjects(generatedObjects);
   }, []);
 
   return (
-    <div className="moving-objects-container absolute top-0 left-0 w-full h-full pointer-events-none">
+    <div className="moving-objects-container">
       {objects.map((obj, index) => (
         <Parallax speed={-Math.random() * 20 - 10} key={index}>
-        <img
-          key={index}
-          src={process.env.PUBLIC_URL + obj.imagePath}
-          style={{
-            animation: `${obj.direction} ${obj.speed}s linear infinite`,
-            transform: `scale(${obj.scale})`,
-            top: obj.top,
-            left: obj.left, 
-            mixBlendMode: 'screen',
-            position: 'absolute',
-          }}
-          className="moving-object"
-          alt=""
-        />
-    </Parallax>
+          <img
+            src={process.env.PUBLIC_URL + obj.imagePath}
+            style={{
+              animation: `${obj.direction} ${obj.speed}s linear infinite`,
+              transform: `scale(${obj.scale})`,
+              top: `${index * 15}%`,
+              left: `${obj.xPos}%`, 
+              opacity: 0.9,
+            }}
+            className="moving-object"
+            alt=""
+          />
+        </Parallax>
       ))}
     </div>
   );
